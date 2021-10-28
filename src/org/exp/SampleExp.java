6f4ef92b55dc234e961e4ab548bccd74b00b39f9
Anitha1120 <anithacse707@gmail.com>
2021-10-28 11:35:12 +0530

    Sample Code

---
+++ src/org/exp/SampleExp.java
@@ -0,0 +1,33 @@
+package org.exp;
+
+public class SampleExp {
+	public static void main(String[] args) {
+		
+		try
+		{
+		
+			System.out.println("1");
+			int sum = 9 / 0;
+			System.out.println("2");
+		}
+		
+		catch (ArithmeticException e) 
+		{
+				System.out.println("3");
+				
+			}
+		
+		 catch (Exception e) 
+		{
+				System.out.println("4");
+			}
+		
+		 finally 
+		 {
+			 System.out.println("5");
+			
+		}
+		
+	}
+
+}
---
+++ src/org/exp/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author A
+ *
+ */
+package org.exp;
